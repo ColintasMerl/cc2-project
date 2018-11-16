1ca43b6fac276a06454abf38bf9ddee3a8c2b256
ColintasMerl <44892687+ColintasMerl@users.noreply.github.com>
2018-11-16 15:49:04 +0800

    Add files via upload

---
+++ SELECTIONSORT.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selection.sort;
+
+/**
+ *
+ * @author cc2_1d-6
+ */
+public class SELECTIONSORT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int[] doSelectionSort(int[] arr) {
+        // TODO code application logic here
+         for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[index]) 
+                    index = j;
+      
+            int smallerNumber = arr[index];  
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+        return arr;
+    }
+     
+    public static void main(String a[]){
+         
+        int[] arr1 = {10,34,2,56,7,67,88,42};
+        int[] arr2 = doSelectionSort(arr1);
+        for(int i:arr2){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+}
